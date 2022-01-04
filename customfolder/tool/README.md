b3af4e3b1615f98d5004e5a843b87113bfa3de43
Wilson <405440065@gms.tku.edu.tw>
2022-01-04 11:32:52 +0800

    feat:complete NXAVG-Utility tool splitdata

---
+++ customfolder/tool/README.md
@@ -0,0 +1,13 @@
+# Splitdata
+If all the photo are in one directory.
+```
+python splitdata.py --data_path <image_directory>
+```
+If your image are in several directory, please execute again, the train.txt and val.txt will be append.
+```
+python splitdata.py --data_path <image_directory_1>
+python splitdata.py --data_path <image_directory_2>
+                            .
+                            .
+                            .
+```
--- customfolder/tool/splitdata.py
+++ customfolder/tool/splitdata.py
@@ -1,21 +1,33 @@
 import os
+import random
+import argparse
+import glob
 
-# the last '/' slash don't forget!!! or it will still training but garbage weight
-path = '/workspace/wilson/dataset/singal_usb_blackBG_noglue_211116/mix_all/'
-cur_path = os.path.abspath(os.getcwd())
-ptlt = os.listdir(path)
-scale = 0.1
-val = int(len(ptlt)//2 * scale)
-print(val)
+def parser():
+    parser = argparse.ArgumentParser(description="split data to train.txt val.txt")
+    parser.add_argument('-d', "--data_path", type=str,
+                        help="please give the image directory")
+    return parser.parse_args()
 
-img_files = []
-for i in range(len(ptlt)):
-    if ptlt[i].split('.')[-1] == "png":
-        img_files.append(ptlt[i])
-with open("train.txt", "w+") as t:
-    with open("val.txt", "w+") as v:
-        for i in range(len(img_files)):
-            if i <= val :
-                    v.writelines([path+img_files[i],'\n'])
-            else:
-                    t.writelines([path+img_files[i],'\n'])
+def check_arguments_errors(args):
+    if not os.path.exists(args.data_path):
+        raise(ValueError("Invalid data_path {}".format(os.path.abspath(args.data_path))))
+
+def main():
+    args = parser()
+    check_arguments_errors(args)
+    files = [filepath for filepath in glob.iglob(os.path.join(args.data_path, '*.png'))]
+    random.shuffle(files)
+    cut = int(len(files)*0.9)
+    arr1 = files[:cut]
+    arr2 = files[cut:]
+
+    with open('train.txt', 'a+') as t:
+        with open('val.txt', 'a+') as v:
+            for i in arr1:
+                t.writelines(i + '\n')
+            for i in arr2:
+                v.writelines(i + '\n')
+
+if __name__ == '__main__':
+    main()
