9874cf6796716ca8b7fade292b20a7783205a1e5
Wilson-TKU <405440065@gms.tku.edu.tw>
2022-01-11 14:25:28 +0800

    feat: add ratio and better the code

--- customfolder/tool/splitdata.py
+++ customfolder/tool/splitdata.py
@@ -2,10 +2,13 @@ import os
 import random
 import argparse
 import glob
+from pathlib import Path
 
 def parser():
     parser = argparse.ArgumentParser(description="split data to train.txt val.txt")
     parser.add_argument('-ip', "--image_path", type=str,
+                        help="please give the image directory")
+    parser.add_argument('-r', "--ratio", type=float, default=0.9,
                         help="please give the image directory")
     return parser.parse_args()
 
@@ -16,18 +19,16 @@ def check_arguments_errors(args):
 def main():
     args = parser()
     check_arguments_errors(args)
-    files = [filepath for filepath in glob.iglob(os.path.join(args.image_path, '*.png'))]
+
+    files = [str(path) for path in Path(args.image_path).rglob('*.png')]
     random.shuffle(files)
-    cut = int(len(files)*0.9)
+    cut = int(len(files)*args.ratio)
     arr1 = files[:cut]
     arr2 = files[cut:]
 
-    with open('train.txt', 'a+') as t:
-        with open('val.txt', 'a+') as v:
-            for i in arr1:
-                t.writelines(i + '\n')
-            for i in arr2:
-                v.writelines(i + '\n')
-
+    with open('train.txt', 'w+') as train, open('val.txt', 'w+') as val:
+        train.writelines('\n'.join(s for s in arr1))
+        val.writelines('\n'.join(s for s in arr2))
+    
 if __name__ == '__main__':
     main()
